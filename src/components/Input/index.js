cfb2eaa15a5a24b2f290c3a7d179c256fa0bb67b
Fernando J Prado <f.j.prado@hotmail.com>
2020-08-14 11:35:48 -0300

    Landing Page 1.0.1
    Add inputs on landing page.

---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+import React, { InputHTMLAttributes } from "react";
+import "./styles.css";
+
+export default function Input({ label, name, ...rest }) {
+    return (
+        <div className="input-block">
+            <label htmlFor={name}>{label}</label>
+            <input type="text" id={name} {...rest} />
+        </div>
+    );
+}
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
+import Input from "../../components/Input";
 import "./styles.css";
 import logoImg from "../../assets/images/logo.svg";
 
@@ -13,24 +15,49 @@ export default function Landing() {
                     <img src={logoImg} alt="Diario" />
                     <h2>Termos e condições</h2>
                 </div>
-                <form onSubmit={handleCreateUser}>
+                <form id="new-diary-form" onSubmit={handleCreateUser}>
+                    <fieldset>
+                        <legend>Seus Dados</legend>
+                        <Input
+                            name="name"
+                            label="Como você gostaria de se identificar?"
+                            value="nome"
+                        />
+                        <Input
+                            name="email"
+                            label="Qual seu email?"
+                            value="email"
+                        />
+                        <Input
+                            name="sex"
+                            label="Como você se identifica sexualmente?"
+                            value="sexo"
+                        />
+                        <Input
+                            name="age"
+                            label="Qual a sua idade?"
+                            value="idade"
+                        />
+                    </fieldset>
                     <div className="buttons-container">
                         <button type="submit" className="pacient-button">
                             Iniciar seu Diário
                         </button>
                     </div>
                 </form>
-                <span className="total-connections">
-                    Total de 100 conexões já realizadas{" "}
-                </span>
-                <div className="buttons-container">
-                    <Link to="/login" className="login-button">
-                        Já tem seu Diário?
-                    </Link>
-                    <Link to="/psy-form" className="psy-button">
-                        É um psicólogo?
-                    </Link>
-                </div>
+                <footer>
+                    <span className="total-connections">
+                        Total de 100 conexões já realizadas{" "}
+                    </span>
+                    <div className="buttons-container">
+                        <Link to="/login" className="login-button">
+                            Já tem seu Diário?
+                        </Link>
+                        <Link to="/psy-form" className="psy-button">
+                            É um psicólogo?
+                        </Link>
+                    </div>
+                </footer>
             </div>
         </div>
     );
