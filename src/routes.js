91ae763f39ff42f326409878d47eb82aff4b797f
Fernando J Prado <f.j.prado@hotmail.com>
2020-08-17 16:23:30 -0300

    Revert "Revert "Revert "Login page mobile layout"""
    This reverts commit c1e7961f24e43e652b8fe87c5bd88b28ddeed283.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Login from "./pages/Login";
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact component={Landing} />
-            <Route path="/login" exact component={Login} />
         </BrowserRouter>
     );
 }
