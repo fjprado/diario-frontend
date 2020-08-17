bc3283b5ba4dc06c13f840586b41dbcf9bf80a37
Fernando J Prado <f.j.prado@hotmail.com>
2020-08-17 16:19:13 -0300

    Login page mobile layout

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Login from "./pages/Login";
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact component={Landing} />
+            <Route path="/login" exact component={Login} />
         </BrowserRouter>
     );
 }
